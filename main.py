8fe036936cfe9389aee6bf5a161f935cd2688a36
Eric Beaulne <eric.minecraft20@gmail.com>
2022-02-28 11:18:59 -0500

    Added Comments-ish

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ async def on_ready():
     print("Online")
 
 ## Load All Cogs
-for file1 in os.listdir("."):
+for file1 in os.listdir("."): # Loop through all files in the current directory
     print("1 " + file1)
-    if os.path.isdir(os.path.join(os.path.abspath("."), file1)):
-        for file2 in os.listdir(os.path.join(os.path.abspath("."), file1)):
+    if os.path.isdir(os.path.join(os.path.abspath("."), file1)): # If the file is a directory
+        for file2 in os.listdir(os.path.join(os.path.abspath("."), file1)):  
             print("2\t" + file2)
-            if file2.endswith(".py"):
+            if file2.endswith(".py"): # If the file is a .py file load it as a cog
                 print("Loading: " + file2)
                 client.load_extension(f"{file1}.{file2[:-3]}")
-            elif os.path.isdir(os.path.join(os.path.join(os.path.abspath("."), file1), file2)):
-                for file3 in os.listdir(os.path.join(os.path.join(os.path.abspath("."), file1), file2)):
-                    if file3.endswith(".py"):
+            elif os.path.isdir(os.path.join(os.path.join(os.path.abspath("."), file1), file2)): # If the file is a directory
+                for file3 in os.listdir(os.path.join(os.path.join(os.path.abspath("."), file1), file2)): # Loop through all files in that directory
+                    if file3.endswith(".py"): # If the file is a .py file load it as a cog
                         print("3\t\t" + file3)
                         print("Loading: " + file3)
                         client.load_extension(f"{file1}.{file2}.{file3[:-3]}")
