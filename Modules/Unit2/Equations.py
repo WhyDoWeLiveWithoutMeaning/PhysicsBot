74c389ed771e3ea5dd7fdd2d64a1be042caca1b5
Eric Beaulne <eric.minecraft20@gmail.com>
2022-02-28 11:17:11 -0500

    Uniform Equations Added

--- Modules/Unit2/Equations.py
+++ Modules/Unit2/Equations.py
@@ -18,13 +18,38 @@ class Equation(commands.Cog):
     async def ueq(
             self, 
             ctx,
-            v1: Option(float, "Initial Velocity in m/s", required=False),
-            v2: Option(float, "Final Velocity in m/s", required=False),
-            d: Option(float, "Displacement in m", required=False),
-            a: Option(float, "Acceleration in m/s^2", required=False),
-            t: Option(float, "Time in s", required=False),
+            v1: Option(float, "Initial Velocity in m/s", required=False) = None,
+            v2: Option(float, "Final Velocity in m/s", required=False) = None,
+            d: Option(float, "Displacement in m", required=False) = None,
+            a: Option(float, "Acceleration in m/s^2", required=False) = None,
+            t: Option(float, "Time in s", required=False) = None,
             ):
-        print(v1, v2, d, a, t)
+        if v1 and a and t:
+            v2 = v1 + a*t
+            d = v1*t + 0.5*a*t**2
+        elif v1 and v2 and d:
+            a = (v2 - v1)/t
+            t = d/v1 - 0.5*a*t
+        elif v1 and v2 and a:
+            t = (v2 - v1)/a
+            d = v1*t + 0.5*a*t**2
+        elif v1 and d and a:
+            t = d/v1 - 0.5*a*t
+            v2 = v1 + a*t
+        elif v1 and d and t:
+            v2 = v1 + a*t
+            a = (v2 - v1)/t
+        elif v1 and v2 and t:
+            a = (v2 - v1)/t
+            d = v1*t + 0.5*a*t**2
+        embed = discord.Embed(title="Uniform Equation", description="", color=0x00ff00)
+        embed.add_field(name="Initial Velocity", value=f"{v1} m/s", inline=False)
+        embed.add_field(name="Final Velocity", value=f"{v2} m/s", inline=False)
+        embed.add_field(name="Acceleration", value=f"{a} m/s^2", inline=False)
+        embed.add_field(name="Time", value=f"{t} s", inline=False)
+        embed.add_field(name="Displacement", value=f"{d} m", inline=False)
+        await ctx.respond(embed=embed)
+        
 
 def setup(client):
     client.add_cog(Equation(client))
